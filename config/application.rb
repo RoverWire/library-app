57697d15c36c14a9309c84543a94a04fe3f7a547
Luis Felipe Perez <slashcat@gmail.com>
2026-04-19 00:52:34 -0600

    global config update to use uuid on migrations

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,9 @@ module LibraryApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
