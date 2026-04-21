cd44cd114797e4f2fcf756bfb8a74ffbd6ac5a38
Luis Felipe Perez <slashcat@gmail.com>
2026-04-20 22:30:19 -0600

    set book copies counter cache

--- app/models/book_copy.rb
+++ app/models/book_copy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BookCopy < ApplicationRecord
-  belongs_to :book
+  belongs_to :book, counter_cache: :copies_count
   has_many :loans, dependent: :delete_all
 
   validates :status, presence: true
