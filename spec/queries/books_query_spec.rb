331e52290feca22bc13b818129487fb878bf2fd0
Luis Felipe Perez <slashcat@gmail.com>
2026-04-21 00:37:40 -0600

    set rubocop multiple memoized helper to 8

--- spec/queries/books_query_spec.rb
+++ spec/queries/books_query_spec.rb
@@ -3,28 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe BooksQuery do
-  describe '.call' do # rubocop:disable RSpec/MultipleMemoizedHelpers
+  describe '.call' do
     subject(:result) { described_class.call(params) }
 
-    let!(:genre_fantasy) { create(:genre, name: 'Fantasy') }
-    let!(:genre_scifi) { create(:genre, name: 'Sci-Fi') }
+    let!(:fantasy) { create(:genre, name: 'Fantasy') }
+    let!(:scifi)   { create(:genre, name: 'Sci-Fi') }
 
-    let!(:author_rowling) { create(:author, first_name: 'J.K.', last_name: 'Rowling') }
-    let!(:author_tolkien) { create(:author, first_name: 'J.R.R.', last_name: 'Tolkien') }
+    let!(:rowling) { create(:author, first_name: 'J.K.', last_name: 'Rowling') }
+    let!(:tolkien) { create(:author, first_name: 'J.R.R.', last_name: 'Tolkien') }
 
-    let!(:book_one) do
-      create(:book, title: 'Harry Potter', author: author_rowling, genre: genre_fantasy)
-    end
+    let!(:book_one) { create(:book, title: 'Harry Potter', author: rowling, genre: fantasy) }
+    let!(:book_two) { create(:book, title: 'Lord of the Rings', author: tolkien, genre: fantasy) }
+    let!(:book_three) { create(:book, title: 'Dune', author: tolkien, genre: scifi) }
 
-    let!(:book_two) do
-      create(:book, title: 'Lord of the Rings', author: author_tolkien, genre: genre_fantasy)
-    end
-
-    let!(:book_three) do
-      create(:book, title: 'Dune', author: author_tolkien, genre: genre_scifi)
-    end
-
-    context 'without filters' do # rubocop:disable RSpec/MultipleMemoizedHelpers
+    context 'without filters' do
       let(:params) { {} }
 
       it 'returns all books' do
@@ -32,7 +24,7 @@ RSpec.describe BooksQuery do
       end
     end
 
-    context 'when filtering by title' do # rubocop:disable RSpec/MultipleMemoizedHelpers
+    context 'when filtering by title' do
       let(:params) { { title: 'Harry' } }
 
       it 'returns matching books' do
@@ -40,7 +32,7 @@ RSpec.describe BooksQuery do
       end
     end
 
-    context 'when filtering by author' do # rubocop:disable RSpec/MultipleMemoizedHelpers
+    context 'when filtering by author' do
       let(:params) { { author: 'Tolkien' } }
 
       it 'returns books from that author' do
@@ -48,7 +40,7 @@ RSpec.describe BooksQuery do
       end
     end
 
-    context 'when filtering by genre' do # rubocop:disable RSpec/MultipleMemoizedHelpers
+    context 'when filtering by genre' do
       let(:params) { { genre: 'Fantasy' } }
 
       it 'returns books in that genre' do
@@ -56,7 +48,7 @@ RSpec.describe BooksQuery do
       end
     end
 
-    context 'when filtering by status' do # rubocop:disable RSpec/MultipleMemoizedHelpers
+    context 'when filtering by status' do
       let(:params) { { status: 'active' } }
 
       it 'returns only active books' do
@@ -64,7 +56,7 @@ RSpec.describe BooksQuery do
       end
     end
 
-    context 'when filtering by title and genre' do # rubocop:disable RSpec/MultipleMemoizedHelpers
+    context 'when filtering by title and genre' do
       let(:params) { { title: 'Lord', genre: 'Fantasy' } }
 
       it 'returns filtered combination' do
@@ -72,7 +64,7 @@ RSpec.describe BooksQuery do
       end
     end
 
-    context 'when filtering by case insensitive search' do # rubocop:disable RSpec/MultipleMemoizedHelpers
+    context 'when filtering by case insensitive search' do
       let(:params) { { title: 'harry' } }
 
       it 'matches regardless of case' do
@@ -80,7 +72,7 @@ RSpec.describe BooksQuery do
       end
     end
 
-    context 'when no matches are found' do # rubocop:disable RSpec/MultipleMemoizedHelpers
+    context 'when no matches are found' do
       let(:params) { { title: 'Nonexistent' } }
 
       it 'returns empty relation' do
