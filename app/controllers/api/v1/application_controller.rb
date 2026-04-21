f15c137398b2b11d99fb2d637f1c4235a7d9bcc1
Luis Felipe Perez <slashcat@gmail.com>
2026-04-20 22:24:09 -0600

    define ability

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -3,9 +3,15 @@
 module Api
   module V1
     class ApplicationController < ActionController::API
+      include CanCan::ControllerAdditions
+
       before_action :authenticate_user!
 
       rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+      rescue_from CanCan::AccessDenied do
+        render json: { error: 'Forbidden' }, status: :forbidden
+      end
 
       respond_to :json
 
---
+++ app/models/ability.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # Define abilities for the user here. For example:
+    #
+    #   return unless user.present?
+    #   can :read, :all
+    #   return unless user.admin?
+    #   can :manage, :all
+    #
+    # The first argument to `can` is the action you are giving the user
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on.
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, published: true
+    #
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    user ||= User.new
+
+    if user.admin?
+      admin_rules(user)
+    elsif user.librarian?
+      librarian_rules(user)
+    elsif user.member?
+      member_rules(user)
+    end
+  end
+
+  def admin_rules(_user)
+    can :manage, :all
+  end
+
+  def librarian_rules(_user)
+    can :manage, Book
+    can :manage, Author
+    can :manage, Genre
+    can :manage, BookCopy
+    can :read, Loan
+    can :update, Loan, returned_at: nil
+    cannot :destroy, Loan
+  end
+
+  def member_rules(user)
+    can :read, Book
+    can :read, Author
+    can :read, Genre
+    can :create, Loan
+    can :read, Loan, user_id: user.id
+    can :update, Loan, user_id: user.id, returned_at: nil
+  end
+end
